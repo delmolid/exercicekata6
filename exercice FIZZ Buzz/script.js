9542bfc08ec571988f9bb0bc03b676838546e690
Molid <delmolid@hotmail.fr>
2025-05-22 15:52:02 +0200

    resolution exercice fizzbuzz premier régle answer[i] == FizzBuzz si i est divisible par 3 ET 5. answer[i] == Buzz si i est divisible uniquement par 5.

--- exercice FIZZ Buzz/script.js
+++ exercice FIZZ Buzz/script.js
@@ -1,14 +1,16 @@
-
-
-
-function fizzBuzz (n) {
-let answer = [];
-for (i = 1; i <= n; i++) {
-if (i % 3 === 0){
-i = "Fizz"
-} 
-answer.push(i)
+function fizzBuzz(n) {
+  let answer = [];
+  for (i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+    answer.push("FizzBuzz");
+    } else if (i % 3 === 0){
+        answer.push("Fizz");
+    } else if (i % 5 === 0){
+       answer.push("Buzz")
+    } else { 
+    answer.push(i.toString())
+    }
+  }
+  console.log(answer);
 }
-console.log(answer) 
-}
-fizzBuzz (3)
+fizzBuzz(15);
